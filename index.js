5f78e237698d8c64a6a81cc0c9f9ebce4be4ae90
Abdur Rehman <your.email@example.com>
2025-06-07 02:05:45 +0500

    ass serverless

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ['https://frontend-alpha-seven-16.vercel.app', 'http://localhost:3000'],
+    origin: ['https://frontend-alpha-seven-16.vercel.app'],
     credentials: true,
   })
 );
